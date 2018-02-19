d4de25d5b8f52d6979f991438bcaf20fc32c1a93
MikaVainio <koti.mika.vainio@gmail.com>
2018-02-19 22:58:27 +0200

    Kirjastopäivityksiä

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const fs = require('fs');
 // Luodaan palvelin
 const server = express();
 
-server.get('/',(request, response) => response.send('Hippopotamus'))
-server.get('/About',(request, response) => response.send('about'))
+// Määritellään juuri URL:n toiminnoksi tekstin lähetys
+server.get('/',(request, response) => response.send('Hippopotamus'));
+// Määritellään About URL:n toiminnoksi tiedoston lähetys
+server.get('/About',(request, response) => {
+  response.sendFile(__dirname +'/README.md');
+});
+
+// Luodaan kuuntelija ja tulostetaan sen tiedot konsoliin
 server.listen(4000, () => console.log('Palvelin kuuntelee porttia 4000'))
