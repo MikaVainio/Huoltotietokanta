b7cdfd7c87fed1a493ce97250a9a82ef0338bba3
MikaVainio <koti.mika.vainio@gmail.com>
2018-02-24 20:35:22 +0200

    Vikiesimerkkien muokkauksia

---
+++ Docs/VikiSnippets/mutable.js
@@ -0,0 +1,5 @@
+// Määritellään vakio serviceTech
+const serviceTech = {name: 'Jakke Jäynä', id: 15};
+// Muutetaan (mutatoidaan) ominaisuutta name
+serviceTech.name = 'Jakke Hyväusko';
+console.log(serviceTech);
