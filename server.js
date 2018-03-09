134d967bfb065bcc6c2e02c39e7dd7e4605ae3ad
MikaVainio <koti.mika.vainio@gmail.com>
2018-03-09 23:03:03 +0200

    Valmisteltu 2 staattinen sivuharjoitus
    Harjoitellaan sivun tekemistä ja lähettämistä Express-kirjaston avulla

--- server.js
+++ server.js
@@ -7,11 +7,21 @@ const fs = require('fs');
 // Luodaan palvelin
 const server = express();
 
+/* Määritellään polku staattisiin tiedostoihin. Kuvat tms. sivuilla esiintyvät
+elementit tallennetaan public-nimiseen hakemistoon tai sen alihakemistoihin */
+server.use(express.static(__dirname + '/public'));
+
 // Määritellään juuri URL:n toiminnoksi tekstin lähetys
-server.get('/',(request, response) => response.send('Hippopotamus'));
-// Määritellään About URL:n toiminnoksi tiedoston lähetys
-server.get('/About',(request, response) => {
+server.get('/', (request, response) => response.send('Hippopotamus'));
+
+// Määritellään About URL:n toiminnoksi md-tiedoston lähetys
+server.get('/About', (request, response) => {
   response.sendFile(__dirname + '/README.md');
+});
+
+// Määritellään Info URL:n toiminnoksi html-tiedoston lähetys
+server.get('/Info', (request, response) => {
+  response.sendFile(__dirname + '/info.html');
 });
 
 // Luodaan kuuntelija ja tulostetaan sen tiedot konsoliin
